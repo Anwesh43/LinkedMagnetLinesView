f6847179820cec1717e83d4bde386b63e2dbbd81
Anwesh43 <anweshthecool0@gmail.com>
2019-05-02 10:24:21 +0530

    adding constants and libraries

--- magnetlinesview/src/main/java/com/anwesh/uiprojects/magnetlinesview/MagnetLinesView.kt
+++ magnetlinesview/src/main/java/com/anwesh/uiprojects/magnetlinesview/MagnetLinesView.kt
@@ -1,5 +1,23 @@
 package com.anwesh.uiprojects.magnetlinesview
 
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.app.Activity
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+
 /**
  * Created by anweshmishra on 02/05/19.
  */
+
+val lines : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val nodes : Int = 5
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
+
