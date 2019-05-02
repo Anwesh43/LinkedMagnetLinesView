becca60cd89a5ee20f6d6c87de09eb2679f17768
Anwesh43 <anweshthecool0@gmail.com>
2019-05-02 10:50:34 +0530

    adding MLNode class

--- magnetlinesview/src/main/java/com/anwesh/uiprojects/magnetlinesview/MagnetLinesView.kt
+++ magnetlinesview/src/main/java/com/anwesh/uiprojects/magnetlinesview/MagnetLinesView.kt
@@ -125,4 +125,48 @@ class MagnetLinesView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class MLNode(var i : Int, val state : State = State()) {
+
+        private var next : MLNode? = null
+        private var prev : MLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = MLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawMLNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : MLNode {
+            var curr : MLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
