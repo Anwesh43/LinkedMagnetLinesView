79592b261925b1bf780133a45fdb3d17fd5e31f2
Anwesh43 <anweshthecool0@gmail.com>
2019-05-02 10:35:28 +0530

    adding drawMLNode canvas extension method

--- magnetlinesview/src/main/java/com/anwesh/uiprojects/magnetlinesview/MagnetLinesView.kt
+++ magnetlinesview/src/main/java/com/anwesh/uiprojects/magnetlinesview/MagnetLinesView.kt
@@ -20,6 +20,7 @@ val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#673AB7")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val parts : Int = 2
 
 fun Int.inverse() : Float = 1f / this
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
@@ -31,3 +32,32 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
 }
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
 
+fun Canvas.drawMagnetLine(i :Int, scale : Float, size : Float, sif : Float, paint : Paint) {
+    val sf : Float = 1f - 2 * i
+    save()
+    translate(-size + i * 2 * size, 0f)
+    rotate(-90f * sf * scale.divideScale(i, parts) * sif)
+    drawLine(0f, 0f, size * sf, 0f, paint)
+    restore()
+}
+
+fun Canvas.drawMLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (lines + 1)
+    val size : Float = gap / sizeFactor
+    val sif : Float = 1f - 2 * (i % 2)
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(gap * (i + 1), h / 2 - (h / 2) * sif * sc2)
+    for (j in 0..(lines - 1)) {
+        save()
+        drawMagnetLine(j, sc1, size, sif, paint)
+        restore()
+    }
+    restore()
+}
