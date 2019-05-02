4523d9fb2dea5f827830a0c5dba7486b0d28d86d
Anwesh43 <anweshthecool0@gmail.com>
2019-05-02 11:17:07 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedmagnetlinesview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedmagnetlinesview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedmagnetlinesview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.magnetlinesview.MagnetLinesView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         MagnetLinesView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
